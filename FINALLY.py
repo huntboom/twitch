c70ae4229075e2bbda06285b0f254c9bf49e4dcc
huntboom <hkarsseboom@hotmail.com>
2021-01-17 12:37:59 -0500

    Removed bloat comments and changed the uploader to use selenium instead of pyautogui

--- FINALLY.py
+++ FINALLY.py
@@ -35,8 +35,7 @@ for item in top_game['data']:
         # top_clip_url=top_clip['url']headers={"Client-ID": client_id,'Authorization': 'Bearer gwct88d9osd6cx4fwultixvcamt3wk'}
         # print(top_clip)
         headers = {"Client-ID": client_id, 'Authorization': authorizationtoken}
-        #client_id = 'hqc99l0g1pv0qhy797w90t5jv04nrd'
-        basepath = '/home/hunter/PycharmProjects/VideoAutomation/downloads/'
+        basepath = '/home/hunter/PycharmProjects/VideoAutomation/TwitchGithub/downloads/'
         # slug = clip.rpartition('/')[-1]
 
         clip_info = requests.get("https://api.twitch.tv/helix/clips?id=" + slug, headers={"Client-ID": client_id,'Authorization':authorizationtoken}).json()
@@ -77,63 +76,63 @@ for item in top_game['data']:
             clips.append(VideoFileClip(filename))
 
     video = concatenate_videoclips(clips, method='compose')
-    video.write_videofile('CurrentVideo.mp4')
+    video.write_videofile('CompiledClips.mp4')
     print('Uploading edited video to YouTube')
     from selenium import webdriver
     if __name__ == "__main__":
+        if os.path.isdir("/home/ubuntu/"):
+            from pyvirtualdisplay import Display
+            display = Display(visible=0,size=(800, 600))
         import time
-
-        driver = webdriver.Chrome()
-        driver.get("https://accounts.google.com/signin/v2/identifier?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den%26next%3Dhttps%253A%252F%252Fwww.youtube.com%252F&hl=en&ec=65620&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
-        driver.find_element_by_xpath("//input[@type='email']").click()
-        driver.find_element_by_xpath("//input[@type='email']").send_keys("automate6969@gmail.com")
-        driver.find_element_by_xpath("//div[@id='identifierNext']").click()
-        time.sleep(4)
-    import pyautogui
-    pyautogui.write("cooldogmaui123")
-    time.sleep(4)
-    pyautogui.click(677,522)
-    time.sleep(4)
-    driver.get("https://studio.youtube.com/channel/UCR2MUX5Xzq552N9PArz7qxg/videos/upload?filter=%5B%5D&sort=%7B%22columnType%22%3A%22date%22%2C%22sortOrder%22%3A%22DESCENDING%22%7D")
-    time.sleep(4)
-    pyautogui.click(921,206)
-    time.sleep(4)
-    pyautogui.click(891,245)
+    driver = webdriver.Chrome()    
+    driver.get('https://studio.youtube.com')
     time.sleep(2)
-    pyautogui.click(533,580)
-    time.sleep(4)
-    pyautogui.write("/home/hunter/PycharmProjects/VideoAutomation/downloads/CurrentVideo.mp4")
-    pyautogui.press('enter')
-    time.sleep(1)
-    pyautogui.press('enter')
-    time.sleep(1)
-    pyautogui.press('enter')
-    time.sleep(5)
     from datetime import date
-    today = "Top "+top_game_name+  " Highlights "+ str(date.today())
-    pyautogui.write(today)
-    pyautogui.click(953,688)
-    time.sleep(3)
-    pyautogui.click(114,554)
+    today = "Top "+top_game_name+  " Highlights of November 27th"
+    driver.find_element_by_xpath("//input[@type='email']").send_keys('automate6969@gmail.com')
     time.sleep(2)
-    pyautogui.click(953,688)
+    driver.find_element_by_xpath("//div[@class='VfPpkd-RLmnJb']").click()
     time.sleep(2)
-    pyautogui.click(953,688)
+    driver.find_element_by_xpath("//input[@type='password']").send_keys('cooldogmaui123')
     time.sleep(2)
-    pyautogui.click(166,600)
+    driver.find_element_by_xpath("//div[@class='VfPpkd-RLmnJb']").click()
     time.sleep(2)
-    pyautogui.click(951,691)
-    print('Waiting for video to finish processing before deleting video...')
-    time.sleep(100)
+    driver.get('https://www.youtube.com/upload')
+    time.sleep(2)
+    elem = driver.find_element_by_xpath("//input[@type='file']")
+    elem.send_keys("/home/hunter/PycharmProjects/VideoAutomation/downloads/CompiledClips.mp4")
+    time.sleep(5)
+    driver.find_element_by_xpath("//div[@id='textbox']").send_keys("Name of Video")
+    time.sleep(5)
+    driver.find_element_by_xpath("//paper-radio-button[@name='NOT_MADE_FOR_KIDS']").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//ytcp-button[@id='next-button']").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//ytcp-button[@id='next-button']").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//paper-radio-button[@name='PUBLIC']").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//ytcp-button[@id='done-button']").click()
+    time.sleep(2)
+    print('closing selenium webbrowser')
+    driver.close()
+
+    #+ str(date.today())
+    #print('Waiting for video to finish processing before deleting video...')
+    #time.sleep(400)
+    #driver.close()
     print('Deleting video and clips now.')
     import os
-    os.remove("CurrentVideo.mp4")
+    os.remove("CompiledClips.mp4")
     import shutil
     #shutil.rmtree("/home/hunter/PycharmProjects/VideoAutomation/downloads")
     #print(top_game_video)
     #if not os.path.exists('downloads'):
         #os.makedirs('downloads')
-    for file in os.scandir('/home/hunter/PycharmProjects/VideoAutomation/downloads'):
+    for file in os.scandir('/home/hunter/PycharmProjects/VideoAutomation/TwitchGithub/downloads'):
         if file.name.endswith(".mp4"):
             os.unlink(file.path)
-    os.chdir('/home/hunter/PycharmProjects/VideoAutomation')
+    os.chdir('/home/hunter/PycharmProjects/VideoAutomation/TwitchGithub')
+    #print('Waiting for video to finish processing before deleting video...')
+    #time.sleep(100)
+    
